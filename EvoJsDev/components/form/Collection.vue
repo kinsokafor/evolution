2ca5770f5faa9d3e2efb60c9b8e01422a41ba74b
Kowebtech Ict <info@koweb.com.ng>
2023-09-20 22:06:15 +0100

    Update

--- EvoJsDev/components/form/Collection.vue
+++ EvoJsDev/components/form/Collection.vue
@@ -61,12 +61,14 @@
         values: Object
     })
     const control = ref(0)
-    const rows = computed(() => {
-        if(props.initialValues[props.name] != undefined && Object.values(props.initialValues[props.name]).length > 0) {
-            initRows.value = [...Object.values(props.initialValues[props.name])].length
-        } else initRows.value = props.attrs.rows
-        return initRows.value + control.value
-    })
+    // const rows = computed(() => {
+    //     if(props.initialValues[props.name] != undefined && Object.values(props.initialValues[props.name]).length > 0) {
+    //         initRows.value = [...Object.values(props.initialValues[props.name])].length
+    //     } else initRows.value = props.attrs.rows
+    //     return initRows.value + control.value
+    // })
+
+    const rows = ref(3)
     
     const removeRow = () => {
         if(rows.value > 1) {
