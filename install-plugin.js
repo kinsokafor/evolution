301a2475492f4e283d92bc8f7ca7aac244b2d6b4
Kowebtech Ict <info@koweb.com.ng>
2023-10-09 17:36:00 +0100

    Install plugin 1

--- install-plugin.js
+++ install-plugin.js
@@ -1,14 +1,29 @@
 const shell = require('shelljs')
-if(process.env.npm_config_plugin == undefined) {
-    throw new Error('Please specify the plugin to install npm run install-plugin --plugin=<plugin name>');
+const path = require('path');
+const {mergeConfig} = require('./evo.config');
+
+const plugin = process.env.npm_config_plugin
+if(plugin == undefined) {
+    throw new Error('Please specify the plugin to install npm run install-plugin --plugin=exampleplugin');
 }
+const arr = plugin
+            .replace('https://github.com/', '')
+            .replace('http://github.com/', '')
+            .replace('//github.com/', '')
+            .split('/')
+if(arr.length < 2) {
+    throw new Error('plugin name must include the author\'s name like johndoe/exampleplugin');
+}
+const [author, pluginName] = plugin;
 //clone the repos
 shell.cd('./Public/Modules')
-shell.exec(`git clone https://github.com/kinsokafor/${process.env.npm_config_plugin} ${process.env.npm_config_plugin}`)
+shell.exec(`git clone https://github.com/${author}/${pluginName} ${pluginName}`)
 
 shell.cd('../../EvoJsDev/Modules')
-shell.exec(`git clone https://github.com/kinsokafor/${process.env.npm_config_plugin}.js ${process.env.npm_config_plugin}`)
+shell.exec(`git clone https://github.com/${author}/${pluginName}.js ${pluginName}`)
 
 //merge config file
+const pluginConfig = path.resolve(__dirname, `./EvoJsDev/Modules/${pluginName}/config.json`)
+mergeConfig(pluginConfig)
 
 //run the install.php
