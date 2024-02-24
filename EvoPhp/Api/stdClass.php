e7455321ffb8fe7b23e19bafc8971eb7bde5cda8
Kowebtech Ict <info@koweb.com.ng>
2024-02-24 12:58:33 +0100

    Fix on Dynamic Properties deprecation

--- EvoPhp/Api/stdClass.php
+++ EvoPhp/Api/stdClass.php
@@ -4,24 +4,27 @@ namespace EvoPhp\Api;
 /**
  * summary
  */
+
 class stdClass
 {
     /**
      * summary
      */
+    private $properties = [];
+
     public function __construct()
     {
         
     }
 
     public function __set($property, $value) {
-    	$this->$property = $value;
+    	$this->properties[$property] = $value;
     }
 
     /**
      * getting the property of the api
      */
     public function __get($property) {
-    	return $this->$property;
+    	return $this->properties[$property] ?? NULL;
     }
 }
