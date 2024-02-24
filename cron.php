5ddaac449e609077a0a3a85cfefbaa6bb575ba36
Kowebtech Ict <info@koweb.com.ng>
2024-02-24 19:41:00 +0100

    Update cron landing

---
+++ cron.php
@@ -0,0 +1,13 @@
+<?php
+
+if (preg_match('/\.(?:png|jpg|jpeg|css|js|woff|woff2)$/', $_SERVER['REQUEST_URI'])) {
+    return false;
+}
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', realpath(dirname(__FILE__)) . '/');
+
+require_once("EvoPhp/autoload.php");
+
+\EvoPhp\Api\Cron::executeDueJobs();
+?>
