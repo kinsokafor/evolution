5456b0459364b9a9d1222d56c603d85741cf4219
Kowebtech Ict <info@koweb.com.ng>
2024-02-28 11:40:20 +0100

    Cron update

--- cron.php
+++ cron.php
@@ -1,8 +1,8 @@
 <?php
 
-if (preg_match('/\.(?:png|jpg|jpeg|css|js|woff|woff2)$/', $_SERVER['REQUEST_URI'] ?? "")) {
-    return false;
-}
+// if (preg_match('/\.(?:png|jpg|jpeg|css|js|woff|woff2)$/', $_SERVER['REQUEST_URI'] ?? "")) {
+//     return false;
+// }
 
 if ( !defined('ABSPATH') )
 	define('ABSPATH', realpath(dirname(__FILE__)) . '/');
