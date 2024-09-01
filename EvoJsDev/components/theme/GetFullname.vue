a1cd68de45f37694f7b82b53df089fdc211b4e5f
Kowebtech Ict <info@koweb.com.ng>
2024-09-01 16:56:03 +0100

    update

---
+++ EvoJsDev/components/theme/GetFullname.vue
@@ -0,0 +1,22 @@
+<template>
+    <span>
+        {{ getFullname(user) }}
+    </span>
+</template>
+
+<script setup>
+    import { useUsersStore } from '@module/Main/store/users'
+    import { computed } from 'vue'
+    import { getFullname } from '@/helpers'
+
+    const props = defineProps({
+        id: Number
+    })
+
+    const store = useUsersStore()
+    const user = computed(() => store.getUser(props.id))
+</script>
+
+<style lang="scss" scoped>
+
+</style>
